cb909bf01c01c4e640b7dca6afa1212b33c5e0dc
Owusu Francis Buabin <codingfrancis100@gmail.com>
2023-01-14 23:45:59 +0000

    created a single component for the gridbox

--- src/components/homepage/image-grid.js
+++ src/components/homepage/image-grid.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import { Row, Col } from 'antd'
+import './styles.css'
 
 import grid1 from '../../assets/grid-1.png'
 import grid2 from '../../assets/grid-2.jpg'
 import grid3 from '../../assets/grid-3.jpg'
 
+export const GridBox = ({ url, title, text }) => {
+    return (
+        <div className='grid-box'>
+            <img src={url} alt="grid image" style={{ width: '100%', height: 'auto' }} />
+            <div className="overlay">
+                <h1 className='title'>{title}</h1>
+                <p className='text'>{text}</p>
+            </div>
+        </div>
+    );
+}
+
 const ImageGrid = () => {
     return (
-        <Row gutter={[20,0]} >
+        <Row gutter={[20, 0]} >
             <Col span={12}>
-                <img src={grid1} alt="grid image" style={{ width: '100%', height: 'auto'}} />
+                <GridBox url={grid1} title={'Web Design'} text={'view projects'} />
             </Col>
             <Col span={12}>
-                <Row gutter={[0,20]}>
+                <Row gutter={[0, 20]}>
                     <Col span={24}>
                         <img src={grid2} alt="grid image" style={{ width: '100%', height: 'auto' }} />
                     </Col>
